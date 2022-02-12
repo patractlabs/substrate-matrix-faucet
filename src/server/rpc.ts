acb519d6b1e890a6fce0409d5d8087a2d99d8f75
99Kies <1290017556@qq.com>
2022-02-12 22:37:23 +0800

    change rpc way

--- src/server/rpc.ts
+++ src/server/rpc.ts
@@ -1,7 +1,7 @@
 import '@polkadot/api-augment';
 
 import { ApiPromise } from '@polkadot/api';
-import { HttpProvider } from '@polkadot/rpc-provider';
+import { WsProvider } from '@polkadot/rpc-provider';
 
 import { getEnvVariable } from '../utils';
 import { envVars } from './serverEnvVars';
@@ -11,7 +11,7 @@ const injectedTypes = JSON.parse(
   getEnvVariable('INJECTED_TYPES', envVars) as string
 ) as Record<string, string>;
 
-const provider = new HttpProvider(rpcEndpointUrl);
+const provider = new WsProvider(rpcEndpointUrl);
 const types = injectedTypes;
 const apiInstance = new ApiPromise({ provider, types });
 
